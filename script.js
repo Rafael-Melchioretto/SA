45258f9f38d1e1bf0875d7426dcb7bb6750fffac
Rafael-Melchioretto <rafael_melchioretto@estudante.sesisenai.org.br>
2026-05-06 12:01:16 -0300

    Fix - correções na estrutura depois de erros com linkagem do css no index, e atuazliação no JavaScript com email e senha fixos para o funcionario cadastrado

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const form = document.getElementById("formLogin")
+const form = document.getElementById("formLogin");
 
 form.addEventListener("submit", function(e){
-  e.preventDefault()
+  e.preventDefault();
 
-  const email = document.getElementById("email").value
-  const senha = document.getElementById("senha").value
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
 
   if(email === "" || senha === ""){
-alert("Preencha todos os campos")
-return
+    alert("Preencha todos os campos");
+    return;
   }
 
-  if(email === "admin@pampaserra.com" && senha === "123456"){
-window.location.href = "home.html"
-} else {
-alert("Login inválido")
+  if(email === "adminpampaserra@gmail.com" && senha === "admin3605"){
+    window.location.href = "home.html";
+  } else {
+    alert("Login inválido");
   }
-})
+});
