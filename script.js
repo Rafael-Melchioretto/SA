5b94cd79c1ff1c374fff651add8221ed3eba89de
MattosCode12 <enzo_mattos@estudante.sesisenai.org.br>
2026-05-08 11:18:06 -0300

    Feat - Cadastro validado

--- script.js
+++ script.js
@@ -17,3 +17,37 @@ form.addEventListener("submit", function(e){
     alert("Login inválido");
   }
 });
+
+const form = document.getElementById("formLogin");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const email = document.getElementById("email").value.trim();
+  const registro = document.getElementById("Registro").value.trim();
+  const senha = document.getElementById("senha").value.trim();
+  const confirmarSenha = document.getElementById("confirmarSenha").value.trim();
+
+  if (email === "" || registro === "" || senha === "" || confirmarSenha === "") {
+    alert("Preencha todos os campos!");
+    return;
+  }
+
+  if (!email.includes("@") || !email.includes(".")) {
+    alert("Digite um email válido!");
+    return;
+  }
+
+  if (senha.length < 6) {
+    alert("A senha precisa ter no mínimo 6 caracteres!");
+    return;
+  }
+
+  if (senha !== confirmarSenha) {
+    alert("As senhas não são iguais!");
+    return;
+  }
+
+  alert("Cadastro realizado com sucesso!");
+  window.location.href = "home.html";
+});
